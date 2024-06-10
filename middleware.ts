1d59f2e3ca541b4356908ebda38d7ef999593eb1
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-06-10 21:52:46 +0200

    commit

--- middleware.ts
+++ middleware.ts
@@ -4,49 +4,38 @@ import {
 } from "next/server";
 import { createClient } from "@/app/utils/supabase/middleware";
 
+const protectedRedirectRoute = "/home";
+const loginRoute = "/login";
+const rootRoute = "/";
+
 export async function middleware(
-  request: NextRequest
+  req: NextRequest
 ) {
+  const path = new URL(req.url).pathname;
   const { 
     supabase, 
     response 
-  } = await createClient(request);
-
-  const path = new URL(request.url).pathname;
+  } = await createClient(req);
 
   const {
-    data: { 
-      user: session 
-    },
+    data: { user },
     error,
   } = await supabase
-    .auth
-    .getUser();
+  .auth
+  .getUser();
 
   if (error) {
     console.log(error);
   }
 
-  if (!session) {
-    return NextResponse
-      .redirect(
-        new URL(
-          "/login", 
-          request.url
-        )
-      );
+  // Redirect authenticated users from root or login to protected route
+  if (user && (path === rootRoute || user && path === loginRoute)) {
+    return NextResponse.redirect(new URL(protectedRedirectRoute, req.url));
   }
 
-  if (session && (
-    path === "/" || 
-    path === "/login"
-  )) {
-    return NextResponse
-      .redirect(new URL(
-        "/home", 
-        request.url
-      )
-    );
+  // Redirect unauthenticated users from protected routes to login
+  if (!user && ![rootRoute, loginRoute].includes(path.toLowerCase())) {
+    return NextResponse.redirect(new URL(loginRoute, req.url));
   }
 
   return response;
