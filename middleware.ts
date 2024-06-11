4693b288bb818072c7f86238cab3f506418b4772
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-06-12 00:38:16 +0200

    commit

--- middleware.ts
+++ middleware.ts
@@ -16,13 +16,9 @@ export async function middleware(
   } = createClient(request);
 
   const path = new URL(request.url).pathname;
-
   const isProtectedRoute = path !== routes.root && path !== routes.login;
 
-  if (!isProtectedRoute) {
-    return response;
-  }
-
+  // Get user session
   const { 
     data: { 
       user: session 
@@ -32,22 +28,24 @@ export async function middleware(
     .getUser()
 
   if (!session) {
-    return NextResponse.redirect(
-      new URL(routes.login, request.url)
-    );
+    // Unauthenticated user logic
+    if (isProtectedRoute) {
+      return NextResponse.redirect(new URL(routes.login, request.url));
+    }
+    // Allow access to root and login routes for unauthenticated users
+    return response;
+  } else {
+    // Authenticated user logic
+    if (!isProtectedRoute) {
+      return NextResponse.redirect(new URL(routes.home, request.url));
+    }
+    // Allow access to protected routes for authenticated users
+    return response;
   }
-
-  if (session && (
-    path === routes.login || path === routes.root
-  )) {
-    return NextResponse.redirect(
-      new URL(routes.home, request.url)
-    );
-  }
-
-  return response;
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|images|favicon.ico|login).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|images|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|bmp|tiff)$).*)",
+  ],
 };
